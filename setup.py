72fcd521f7478ae8846c8b83f47b3438d4ccba8f
Chad Shryock <chad@g3rdmedia.com>
2013-07-28 13:27:01 -0400

    base setup file

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from timetable import VERSION
+
+github_url = 'https://github.com/g3rd/django-timetable'
+
+setup(
+    name='django-timetable',
+    version='.'.join(str(v) for v in VERSION),
+    description='An Django app that provides generic calendar functions',
+    long_description=open('README.md').read(),
+    url=github_url,
+    author='Chad Shryock',
+    author_email='chad@g3rdmedia.com',
+    requires=[
+        'Django (>=1.5)',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
